af0651eb4ed003d49cb86daec14b1eaa1a099ea4
kskjf <2956665104@qq.com>
2016-10-28 09:13:52 +0800

    林

--- app/src/main/java/com/example/administrator/kdc/activity/BackActivity.java
+++ app/src/main/java/com/example/administrator/kdc/activity/BackActivity.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.administrator.kdc.R;
@@ -16,75 +17,123 @@ import org.xutils.common.Callback;
 import org.xutils.http.RequestParams;
 import org.xutils.x;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
+import cn.bmob.sms.BmobSMS;
+import cn.bmob.sms.exception.BmobException;
+import cn.bmob.sms.listener.RequestSMSCodeListener;
+import cn.bmob.sms.listener.VerifySMSCodeListener;
 
-public class BackActivity extends AppCompatActivity {
-    private EditText newpwd,newpwd2,number,conf;
-    private Button sunmit,confirm;
 
+public class BackActivity extends AppCompatActivity {//修改过
+    @InjectView(R.id.b_returnsunmit)
+    ImageView bReturnsunmit;
+    private EditText newpwd, newpwd2, number, conf;
+    private Button sunmit, confirm;
+    String APPID = "b196bd5a61fd7201f2bd4684f880c15f";
+    public static String telnumber;//手机号
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_back);
-        sunmit= (Button) findViewById(R.id.b_sunmit);
-        confirm= (Button) findViewById(R.id.b_confirm);
+        BmobSMS.initialize(this, APPID);//初始化
+        ButterKnife.inject(this);
+        sunmit = (Button) findViewById(R.id.b_sunmit);//提交
+        confirm = (Button) findViewById(R.id.b_confirm);//获取验证码
+        newpwd = (EditText) findViewById(R.id.et_newpwd);//旧密码
+        newpwd2 = (EditText) findViewById(R.id.et_newpwd2);//新密码
+//        number= (EditText) findViewById(R.id.et_number);//电话号码
+//        conf= (EditText) findViewById(R.id.et_confirm);//验证码
+        confirm.setOnClickListener(new View.OnClickListener() {//发送验证码
+            @Override
+            public void onClick(View view) {
+                telnumber = number.getText().toString();//手机号
+                Log.i("bmob", "短信id：" + telnumber);//用于查询本次短信发送详情
+                BmobSMS.requestSMSCode(BackActivity.this, telnumber, "模板名称", new RequestSMSCodeListener() {
 
-        newpwd= (EditText) findViewById(R.id.et_newpwd);
-        newpwd2= (EditText) findViewById(R.id.et_newpwd2);
-        number= (EditText) findViewById(R.id.et_number);
-        conf= (EditText) findViewById(R.id.et_confirm);
+                    @Override
+                    public void done(Integer smsId, BmobException ex) {
+                        // TODO Auto-generated method stub
+                        if (ex == null) {//验证码发送成功
+                            Log.i("bmob", "短信id：" + smsId);//用于查询本次短信发送详情
+                        } else {
+                            Log.i("bmob", "errorCode = " + ex.getErrorCode() + ",errorMsg = " + ex.getLocalizedMessage());
+                        }
+                    }
+                });
 
+
+            }
+        });
         sunmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                String numbers = number.getText().toString();
-                String conf = number.getText().toString();
-                String pwds = newpwd.getText().toString();
-                String pwds2 = newpwd2.getText().toString();
-
-                if(numbers.equals("")||pwds.equals("")||conf.equals("")){
-                    Toast.makeText(BackActivity.this, "您有重要信息未填写，请确认输入", Toast.LENGTH_SHORT).show();
-                }else
-                if (!pwds.equals(pwds2)) {
-                    Toast.makeText(BackActivity.this, "您输入的两次密码并不相同，请重新输入", Toast.LENGTH_SHORT).show();
-                    newpwd.setText("");
-                    newpwd2.setText("");
-                }else{
-
-                    RequestParams params = new RequestParams(NetUtil.url +"BackServlet");
-                    params.addBodyParameter("numbers", numbers);//post方法的传值
-                    params.addBodyParameter("conf", conf);
-                    params.addBodyParameter("pwds", pwds);
-
-                    Toast.makeText(BackActivity.this, "正在修改中，请稍等。。。", Toast.LENGTH_SHORT).show();
-                    x.http().post(params, new Callback.CommonCallback<String>() {//post的方式网络通讯
+                String verifySmsCode = conf.getText().toString();//验证码
+                if (!verifySmsCode.equals("")) {
+                    BmobSMS.verifySmsCode(BackActivity.this, telnumber, verifySmsCode, new VerifySMSCodeListener() {//验证验证码
                         @Override
-                        public void onSuccess(String result) {
-                            Log.e("TAG", "u3  get yes" + result);
-                            if (result.equals("修改成功，跳转至登录页面")) {
-                                Toast.makeText(BackActivity.this,result,Toast.LENGTH_SHORT).show();
-                                Intent intent = new Intent(BackActivity.this, LoginActivity.class);
-                                startActivity(intent);
-                                finish();
-                            }else{
-                                Toast.makeText(BackActivity.this,result,Toast.LENGTH_SHORT).show();
+                        public void done(BmobException ex) {
+                            // TODO Auto-generated method stub
+                            if (ex == null) {//短信验证码已验证成功
+                                Log.i("bmob", "验证通过，正在前往注册");
+                                String pwds = newpwd.getText().toString();
+                                String pwds2 = newpwd2.getText().toString();
+                                if (telnumber.equals("") || pwds.equals("")) {
+                                    Toast.makeText(BackActivity.this, "您有重要信息未填写，请确认输入", Toast.LENGTH_SHORT).show();
+                                } else if (!pwds.equals(pwds2)) {
+                                    Toast.makeText(BackActivity.this, "您输入的两次密码并不相同，请重新输入", Toast.LENGTH_SHORT).show();
+                                    newpwd.setText("");
+                                    newpwd2.setText("");
+                                } else {
+
+                                    RequestParams params = new RequestParams(NetUtil.url + "BackServlet");
+                                    params.addBodyParameter("numbers", telnumber);//post方法的传值
+//                                  params.addBodyParameter("conf", conf);
+                                    params.addBodyParameter("pwds", pwds);
+                                    Toast.makeText(BackActivity.this, "正在修改中，请稍等。。。", Toast.LENGTH_SHORT).show();
+                                    x.http().post(params, new Callback.CommonCallback<String>() {//post的方式网络通讯
+                                        @Override
+                                        public void onSuccess(String result) {
+                                            Log.e("TAG", "u3  get yes" + result);
+                                            if (result.equals("修改成功，跳转至登录页面")) {
+                                                Toast.makeText(BackActivity.this, result, Toast.LENGTH_SHORT).show();
+                                                Intent intent = new Intent(BackActivity.this, LoginActivity.class);
+                                                startActivity(intent);
+                                                finish();
+                                            } else {
+                                                Toast.makeText(BackActivity.this, result, Toast.LENGTH_SHORT).show();
+                                            }
+                                        }
+
+                                        @Override
+                                        public void onError(Throwable ex, boolean isOnCallback) {
+                                            Log.e("TAG", "u3  get no");
+                                        }
+
+                                        @Override
+                                        public void onCancelled(CancelledException cex) {
+                                            Toast.makeText(x.app(), "cancelled", Toast.LENGTH_LONG).show();
+                                        }
+
+                                        @Override
+                                        public void onFinished() {
+                                        }
+                                    });
+                                }
+                            } else {
+                                Log.i("bmob", "验证失败：code =" + ex.getErrorCode() + ",msg = " + ex.getLocalizedMessage());
+                                Toast.makeText(BackActivity.this, "验证码无效，请输入正确验证码", Toast.LENGTH_SHORT).show();
                             }
-                        }
-                        @Override
-                        public void onError(Throwable ex, boolean isOnCallback) {
-                            Log.e("TAG", "u3  get no");
-                        }
-
-                        @Override
-                        public void onCancelled(CancelledException cex) {
-                            Toast.makeText(x.app(), "cancelled", Toast.LENGTH_LONG).show();
-                        }
-                        @Override
-                        public void onFinished() {
                         }
                     });
                 }
             }
         });
+    }
+
+    @OnClick(R.id.b_returnsunmit)
+    public void onClick() {
+        finish();
     }
 }
--- app/src/main/java/com/example/administrator/kdc/activity/LoginActivity.java
+++ app/src/main/java/com/example/administrator/kdc/activity/LoginActivity.java
@@ -1,8 +1,6 @@
 package com.example.administrator.kdc.activity;
 
-import android.content.ContentValues;
 import android.content.Intent;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -13,7 +11,7 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.administrator.kdc.R;
-import com.example.administrator.kdc.db.Mydb;
+import com.example.administrator.kdc.utils.MyApplication;
 import com.example.administrator.kdc.utils.NetUtil;
 import com.example.administrator.kdc.vo.User_tbl;
 import com.example.administrator.kdc.vo.Usershow_tbl;
@@ -24,22 +22,14 @@ import org.xutils.http.RequestParams;
 import org.xutils.x;
 
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {//登陆界面//界面修改过
 
     private EditText name, pwd;
     private Button login, login2, back, registred;
     private User_tbl user = null;
     private ImageView b;
-
-    private Mydb dbHelpr;
-    SQLiteDatabase db;
-
     String pwds;
     int names;
-
-    boolean jizhu;
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,24 +47,61 @@ public class LoginActivity extends AppCompatActivity {
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {//登录
-
                  names = Integer.parseInt(name.getText().toString());
                  pwds = pwd.getText().toString();
 
-                login();
-
+                RequestParams params = new RequestParams(NetUtil.url +"LoginServlet2");
+                params.addBodyParameter("userName", names+"");//post方法的传值
+                params.addBodyParameter("userPwd", pwds);
+                Toast.makeText(LoginActivity.this, "正在登录中，请稍等。。。", Toast.LENGTH_SHORT).show();
+                x.http().post(params, new Callback.CommonCallback<String>() {//post的方式网络通讯
+                    @Override
+                    public void onSuccess(String result) {
+                        Log.e("BBBBB", "u3  get yes+" + result);
+                        Gson gson=new Gson();
+                        Usershow_tbl usershows=new Usershow_tbl();
+                        usershows=gson.fromJson(result,Usershow_tbl.class);
+                        User_tbl users=usershows.getUser_tbl();
+                        if (!usershows.equals(null)) {
+                            Log.d("BBBBB","登录成功");
+                            //登录成功后将
+                            ((MyApplication) getApplication()).setUsershow(usershows);
+                            ((MyApplication) getApplication()).setUser(users);
+                            Toast.makeText(LoginActivity.this, "登录成功，欢迎您", Toast.LENGTH_SHORT).show();
+                           //将user_id传过去
+                            Intent intent=new Intent(LoginActivity.this,HomeActivity.class);
+                            intent.putExtra("user_id",users.getUser_id());
+                            startActivity(intent);
+                        } else {
+                            Log.d("BBBBB","登录失败"+result);
+                        }
+                    }
+                    @Override
+                    public void onError(Throwable ex, boolean isOnCallback) {
+                        Log.e("BBBBB", "u3  get no");
+                        pwd.setText("");
+                        Toast.makeText(LoginActivity.this, "您的账号/密码错误，请区分大小写或者重新输入", Toast.LENGTH_SHORT).show();
+                    }
+                    @Override
+                    public void onCancelled(CancelledException cex) {
+                        Toast.makeText(x.app(), "cancelled", Toast.LENGTH_LONG).show();
+                    }
+                    @Override
+                    public void onFinished() {
+                    }
+                });
             }
         });
         login2.setOnClickListener(new View.OnClickListener() {//游客登录
             @Override
-            public void onClick(View v) {
+            public void onClick(View v) {//主页
                       Intent intent=new Intent(LoginActivity.this,HomeActivity.class);
                      startActivity(intent);
             }
         });
         back.setOnClickListener(new View.OnClickListener() {//找回密码
             @Override
-            public void onClick(View v) {
+            public void onClick(View v) {//忘记密码
 
 
                    Intent intent=new Intent(LoginActivity.this,BackActivity.class);
@@ -84,101 +111,24 @@ public class LoginActivity extends AppCompatActivity {
         });
         registred.setOnClickListener(new View.OnClickListener() {//注册
             @Override
-            public void onClick(View v) {
+            public void onClick(View v) {//注册用户
                 Intent intent=new Intent(LoginActivity.this,RegistredActivity.class);
                 startActivity(intent);
             }
         });
     }
+/*    @Override
+    protected void  onActivityResult(int requestCode,int resultCode,Intent data){
+switch (requestCode){
+    case 1:
+        if(resultCode==RESULT_OK){
 
-
-    public void login(){
-
-        RequestParams params = new RequestParams(NetUtil.url +"LoginServlet2");
-        params.addBodyParameter("userName", names+"");//post方法的传值
-        params.addBodyParameter("userPwd", pwds);
-        Toast.makeText(LoginActivity.this, "正在登录中，请稍等。。。", Toast.LENGTH_SHORT).show();
-        x.http().post(params, new Callback.CommonCallback<String>() {//post的方式网络通讯
-            @Override
-            public void onSuccess(String result) {
-                Log.e("BBBBB", "u3  get yes+" + result);
-                Gson gson=new Gson();
-                Usershow_tbl usershows=new Usershow_tbl();
-                usershows=gson.fromJson(result,Usershow_tbl.class);
-                User_tbl users=usershows.getUser_tbl();
-
-                if (!usershows.equals(null)) {
-                    //将数据存储到数据库里
-                    Log.d("BBBBB","登录成功");
-
-                    dbHelpr=new Mydb(LoginActivity.this,"kdc.db",null,1);
-                    dbHelpr.getWritableDatabase();
-
-                    db=dbHelpr.getWritableDatabase();
-
-                    //清空表
-                    db.delete("user_tbl",null,null);
-                    db.delete("usershow_tbl",null,null);
-
-                    //插入表
-                    ContentValues values=new ContentValues();
-                    values.put("id",users.getUser_id());
-                    values.put("user_number",users.getUser_number());
-                    values.put("user_pwd",users.getUser_pwd());
-                    values.put("user_emal",users.getUser_emal());
-                    db.insert("user_tbl",null,values);
-                    values.clear();
-
-                    values.put("id",usershows.getUsershow_id());
-                    values.put("user_id",usershows.getUser_tbl().getUser_id());
-                    values.put("usershow_name",usershows.getUsershow_name());
-                    values.put("usershow_sex",usershows.getUsershow_sex());
-                    values.put("usershow_age",usershows.getUsershow_age());
-                    values.put("usershow_birthday", String.valueOf(usershows.getUsershow_birthday()));
-                  //  values.put("usershow_birthday", "1994-12-16 12:12");
-                    values.put("usershow_head",usershows.getUsershow_head());
-                    values.put("usershow_credit",usershows.getUsershow_credit());
-                    values.put("usershow_money",usershows.getUsershow_money());
-                    values.put("address_id",usershows.getAddress_id());
-                    db.insert("usershow_tbl",null,values);
-                    values.clear();
-
-            //        Log.d("BBBBB","到达不了????"+result);
-
-                    //登录成功后将
-
-//                    ((MyApplication) getApplication()).setUsershow(usershows);
-//                    ((MyApplication) getApplication()).setUser(users);
-
-                    Toast.makeText(LoginActivity.this, "登录成功，欢迎您", Toast.LENGTH_SHORT).show();
-                    //将user_id传过去
-                    Intent intent=new Intent(LoginActivity.this,HomeActivity.class);
-                    intent.putExtra("user_id",users.getUser_id());
-                    startActivity(intent);
-                    finish();
-
-                } else {
-                //    Log.d("BBBBB","登录失败"+result);
-                }
-            }
-
-            @Override
-            public void onError(Throwable ex, boolean isOnCallback) {
-                Log.e("BBBBB", "u3  get no");
-                pwd.setText("");
-                Toast.makeText(LoginActivity.this, "您的账号/密码错误，请区分大小写或者重新输入", Toast.LENGTH_SHORT).show();
-            }
-            @Override
-            public void onCancelled(CancelledException cex) {
-                Toast.makeText(x.app(), "cancelled", Toast.LENGTH_LONG).show();
-            }
-            @Override
-            public void onFinished() {
-            }
-        });
-
-
-    }
-
+            user = (User_tbl) data.getSerializableExtra("user");
+           // Toast.makeText(LoginActivity.this,"反馈"+user.getUserName(),Toast.LENGTH_SHORT).show();
+            // Log.d("LoginActivity", "book title->" + user.getUserName());
+            name.setText(user.getUserName());
+            pwd.setText(user.getUserPwd());
+        }}
+*/
 
 }
